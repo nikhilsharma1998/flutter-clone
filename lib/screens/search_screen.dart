e966426c2eabc9c4561fe453a0c5fc5df6792d64
nikhilsharma1998 <nikzsharma1998@gmail.com>
2023-08-22 14:38:57 +0530

    new commit

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -95,7 +95,7 @@ class _SearchScreenState extends State<SearchScreen> {
                     (index % 7 == 0) ? 2 : 1,
                     (index % 7 == 0) ? 2 : 1,
                   ),
-                  mainAxisSpacing: 9,
+                  mainAxisSpacing: 10,
                   crossAxisSpacing: 9,
                 );
               },
